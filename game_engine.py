28a9a4174e5962a3c2417039dc53852c57fd4721
Sivler9 <elvisdiaz2005@gmail.com>
2019-10-05 13:21:58 +0200

    Estructura inicial del jugador

--- game_engine.py
+++ game_engine.py
@@ -18,15 +18,20 @@ class Turn:
 
 
 class Phase:
-    """Sub-turno/Fase, contiene la accion que tiene que ejecutar el jugador"""
+    """Sub-turno/Fase, contiene la accion que tiene que ejecutar el jugador
+    coger carta
+    jugar mano
+    fin del turno
+    """
     def __init__(self):
         pass
     pass
 
 
 class Stage:
-    """Fase global, contiene acciones para grupo de jugadores
+    """Fase asincrona, contiene acciones para grupo de jugadores sin orden concreto.
     todo el mundo coge cartas
+    ...
     """
     def __init__(self):
         pass
@@ -70,8 +75,19 @@ class Deck(Pile):
 
 
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, public, private, game_state, pid=0, name='unknown'):
+        self.pid = pid
+        self.name = name
+        self.public = public if public else {}              # public to every player
+        self.private = private if private else {}           # private to this player
+        self.game_state = game_state if game_state else {}  # only redable by game
+    pass
+
+
+class PlayerGroup:
+    def __init__(self, shared_info, shared_state):
+        self.shared_state = shared_state if shared_state else []
+        self.shared_info = shared_info if shared_info else []
     pass
 
 
@@ -85,4 +101,7 @@ class Collectible:
     def __init__(self):
         pass
     pass
+
+
+
 
