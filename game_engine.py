4ef78f57ce887a9d09daaa6a1aea98bdf33a9041
Aleix <algafix@gmail.com>
2019-10-06 05:05:42 +0200

    game engine 80% fet

--- game_engine.py
+++ game_engine.py
@@ -1,15 +1,20 @@
 #!/bin/env python3
 from collections import deque
 import copy
+import random
 
+ESPERANT_RESPOSTA = False
 
 class Game:
     def __init__(self, players=None, rounds=None, board=None, rules=None, event_list=None):
-        class Deck(deque):
-            pass
-
+        
         class Pile:
             pass
+        
+        self.deck = Deck()
+
+        #Cartas que estan en el juego en una ronda
+        self.cartas_jugadas = []
 
         self.players = players if players else [
             self.Player(x, 'test' + str(x), {'active': True}, {'gems': 0}) for x in range(5)
@@ -62,7 +67,7 @@ class Game:
         def execute(self):
             event = self.popleft()
             next_events = event.execute()
-            # print(next_events)
+            #print(next_events)
 
     class Event:
         """Acciones que generan una reaccion
@@ -81,25 +86,80 @@ class Game:
         def execute(self, target=None, **parameters):
             self.function(target if target else self.target, **parameters)
 
-    def main_loop(self):
-        def decrease(target, **kargs):
-            if target is self.board:
-                target.counter -= 1
-                if target.counter > 0:
-                    event_queue.append(self.Event('decrease', '-1', self.board, decrease))
-            else:
-                raise LookupError
-            pass
 
+    # Events del joc que es criden entre ells
+    def main_loop(self):
+                
+        # Primera fase que es crida, test
         def introduction(target, **kargs):
             print('hola')
 
+
+        # Inici del primer torn
         def initial_turn(target, **kargs):
-            print('mundo')
-            event_queue.append(self.Event('decrease', '-1', self.board, decrease))
+            self.createDeck()
+            random.shuffle(self.deck.cards)
+            event_queue.append(self.Event('turn', 'descripcio', self.board, standard_turn))
 
-        # self.deck = Deck()
+        # Torn normal de revelar carta i decidir que fer
+        def standard_turn(target, **kargs):
 
+            carta = self.deck.cards.pop()
+            self.cartas_jugadas.append(carta)
+
+            print(self.cartas_jugadas[0].name)
+
+            if(carta.cardType == "Tresor"):
+                event_queue.append(self.Event('repartir', 'descripcio', self.board, repartir))
+            else:
+                event_queue.append(self.Event('comprovar_estat', 'descripcio', self.board, comprovar_estat))
+
+        # Reparteix els diamants entre els jugadors actius com a diamants de la ronda
+        def repartir(target, **kargs):
+
+            # No entenc com s'han codificat les coses de jugadors
+            # No veig on esta codificat lo de deixar els diamants sobrants a la ruta
+
+            event_queue.append(self.Event("esperar_resposta", "descripcio", self.board, esperar_resposta))
+
+        # Comprova que no hi hagi cap trampa repetida a la llista de cartes jugades
+        def comprovar_estat(target, **kargs):
+            
+            trampa_repetida = False
+            for i in self.cartas_jugadas[:-1]:
+                if(i.name == self.cartas_jugadas[-1].name):
+                    trampa_repetida = True
+                    # Treure els jugadors de la cova i fer perdre diamants
+
+                    # Torna a guardar les cartes menys la trampa repe
+                    self.deck.add(self.cartas_jugadas[:-1])
+                    # Començar la seguent ronda (ficar als jugadors a la nova cova i torn normal)
+                    event_queue.append(self.Event("seguent_ronda", "descripcio", self.board, seguent_ronda))
+                    break
+            
+            # Si ha sortit una trampa per primer cop o una reliquia, es segueix normal
+            if(trampa_repetida == False):
+                event_queue.append(self.Event("esperar_resposta", "descripcio", self.board, esperar_resposta))
+            
+
+        # Espera la resposta dels jugadors de si segueixen o no, el bot.py haura de posar un evento a la cua
+        def esperar_resposta(target, **kargs):
+            ESPERANT_RESPOSTA = True
+            print("ESPERANT JUGADORS")
+
+        # Ficar als jugadors a la nova cova i torn normal
+        def seguent_ronda(target, **kargs):
+            pass
+        
+        # FALTA DEFINIR QUE PASSA QUAN CONTESTEN ELS JUGADORS: 
+        #       REPARTIR RELIQUIES I TREURE DEL DECK
+        #       REPARTIR DIAMANTS ENTRE ELS QUE ES RETIREN
+        #       ACABAR RONDA SI ES RETIREN TOTS
+
+
+
+
+        # Cua d'events
         event_queue = self.EventQueue([
             self.Event('intro', '', None, introduction),
             self.Event('init', '', None, initial_turn)
@@ -169,11 +229,11 @@ class Deck:
     """Group of cards"""
     def add(self, card):
         if type(card) is list:
-            cards = self.cards + card
+            self.cards = self.cards + card
         else:
             self.cards.append(card)
 
-    def __init__(self, cards):
+    def __init__(self, cards=[]):
         self.cards = []
 
 
